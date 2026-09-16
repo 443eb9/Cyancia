ffedc1203073d9b93f6c2a98dba25efcd810de52
443eb9#C <443eb9@gmail.com>
2026-09-16 14:11:13 +0800

    landing dock

--- crates/lapiz_builtin_docks/src/lib.rs
+++ crates/lapiz_builtin_docks/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod brush_preset;
 pub mod canvas;
 pub mod color_selector;
+pub mod landing;
 pub mod layers;
 pub mod tool_box;
 pub mod tool_options;
@@ -11,8 +12,8 @@ use lapiz_dock::DockRegistry;
 use lapiz_runtime::{Application, plugin::Plugin};
 
 use crate::{
-    brush_preset::BrushPresetDock, color_selector::ColorSelectorDock, layers::LayersDock,
-    tool_box::ToolBoxDock, tool_options::ToolOptionsDock,
+    brush_preset::BrushPresetDock, color_selector::ColorSelectorDock, landing::LandingDock,
+    layers::LayersDock, tool_box::ToolBoxDock, tool_options::ToolOptionsDock,
 };
 
 pub struct BuiltinDocksPlugin;
@@ -34,6 +35,7 @@ impl Plugin for BuiltinDocksPlugin {
             registry.register(ToolOptionsDock::new(services));
         }
 
+        registry.register(LandingDock::new());
         registry.register(LayersDock::new());
         registry.register(ToolBoxDock::new());
 
