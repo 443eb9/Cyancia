b7f131b600b8715afff544b450165388c808d190
443eb9#C <443eb9@gmail.com>
2026-09-17 15:29:18 +0800

    Register brush view in plugin

--- crates/lapiz_app/src/main.rs
+++ crates/lapiz_app/src/main.rs
@@ -121,7 +121,6 @@ fn main() {
 
         rt.window_manager_mut().set_root_view::<MainView>();
         rt.window_manager_mut().register_view::<MainView>();
-        rt.window_manager_mut().register_view::<BrushEditor>();
         rt.window_manager_mut().register_view::<FilterPanel>();
         rt.window_manager_mut().register_view::<FilterEditor>();
         rt.window_manager_mut().register_view::<ExportDialogView>();
